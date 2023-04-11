4cf405fe25d4877c5f51c097b90489f5d47b9ea1
Ankush <128672958+uic19bca1130@users.noreply.github.com>
2023-04-11 12:17:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter , Route, Routes} from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import QuizPage from "./components/QuizPage";
+import ResultPage from "./components/ResultPage";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+      <Route path=""element={<LandingPage/>}/>
+        <Route path="LandingPage"element={<LandingPage/>}/>
+        <Route path="QuizPage"element={<QuizPage/>}/>
+        <Route path="ResultPage"element={<ResultPage/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
-
 export default App;
